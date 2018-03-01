49e726921ad187472bf1a12e8d6f3de7e74c985f
Constantin <cdval13@freeuni.edu.ge>
2018-03-01 19:33:15 +0400

    Changed db uri to docker container

--- flask-app/config/settings.py
+++ flask-app/config/settings.py
@@ -1,4 +1,4 @@
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-db_uri = 'postgresql://koka:123@localhost:5432/postgres'
+db_uri = 'postgresql://shareloc:shareloc@localhost:5432/shareloc'
 SQLALCHEMY_DATABASE_URI = db_uri
