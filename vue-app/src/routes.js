934495efc7e5348fa30aefbbaf3ea019aea38e0a
Constantin <cdval13@freeuni.edu.ge>
2018-03-03 19:01:12 +0400

    Added guard for map component

--- vue-app/src/routes.js
+++ vue-app/src/routes.js
@@ -18,24 +18,25 @@ const router = new Router({
       name: 'map',
       component: function (resolve) {
         require(['./components/map-component.vue'], resolve)
-      }
+      },
+      beforeEnter: guardRoute
     }
   ]
 });
 
-// function guardRoute (to, from, next) {
-//   // work-around to get to the Vuex store (as of Vue 2.0)
-//   const auth = router.app.$options.store.state.auth
-//
-//   if (!auth.isLoggedIn) {
-//     next({
-//       path: '/login',
-//       query: { redirect: to.fullPath }
-//     })
-//   } else {
-//     next()
-//   }
-// }
-//
+function guardRoute (to, from, next) {
+
+  var token = localStorage.getItem('token');
+  // TODO check if valid
+  if (token == null) {
+    next({
+      path: '',
+      query: { redirect: to.fullPath }
+    })
+  } else {
+    next()
+  }
+}
+
 export default router
 
