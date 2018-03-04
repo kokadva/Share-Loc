30453b0947c7e623de01950d601b0fe91b73d5b3
Constantin <cdval13@freeuni.edu.ge>
2018-03-04 17:51:11 +0400

    My location tracking implemented

--- vue-app/src/components/map-component.vue
+++ vue-app/src/components/map-component.vue
@@ -3,32 +3,75 @@
 </template>
 
 <script>
-  import Map from 'ol/map';
-  import View from 'ol/view';
-  import TileLayer from 'ol/layer/tile';
-  import XYZ from 'ol/source/xyz';
-  import OSM from 'ol/source/osm';
 
   export default {
     data () {
       return {
         username: '',
-        password: ''
+        password: '',
+        geolocation: null,
+        positionFeature: null,
+        map: null,
+        view: null
       }
     },
+
     mounted: function () {
-      var map = new Map({
+
+      this.view = new ol.View({
+        center: [0, 0],
+        zoom: 2,
+      });
+
+
+      this.positionFeature = new ol.Feature();
+      this.positionFeature.setStyle(new ol.style.Style({
+        image: new ol.style.Circle({
+          radius: 6,
+          fill: new ol.style.Fill({
+            color: '#3399CC'
+          }),
+          stroke: new ol.style.Stroke({
+            color: '#fff',
+            width: 2
+          })
+        })
+      }));
+
+
+      var source = new ol.source.Vector({
+        features: [this.positionFeature]
+      });
+
+      var vector = new ol.layer.Vector({
+        source: source
+      });
+
+      this.map = new ol.Map({
         target: 'map',
         layers: [
-          new TileLayer({
-            source: new OSM()
-          })
+          new ol.layer.Tile({
+            source: new ol.source.OSM()
+          }),
+          vector
         ],
-        view: new View({
-          center: [0, 0],
-          zoom: 2
-        })
+        view: this.view
       });
+
+      this.geolocation = new ol.Geolocation({
+        projection: this.view.getProjection(),
+        tracking: true
+      });
+
+      this.geolocation.on('change:position', this.onChangeLocation);
+
+
+    },
+    methods: {
+      onChangeLocation: function () {
+        var coordinates = this.geolocation.getPosition();
+        this.positionFeature.setGeometry(coordinates ? new ol.geom.Point(coordinates) : null);
+      }
     }
   }
 </script>
